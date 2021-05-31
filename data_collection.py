d125e12dd2e45ff1d60b55763048fdab1d0a1b24
Amar pratap singh <amarpratap2208@gmail.com>
2021-06-01 00:30:12 +0530

    Can search for most pages now.

--- data_collection.py
+++ data_collection.py
@@ -1,65 +1,85 @@
 import requests
 from bs4 import BeautifulSoup
-import re
 
-def get_html(article:str):
-    # Getting the response object using the article defined above.
-    res = requests.get('https://en.wikipedia.org/wiki/'+article)
-    article_html = res.content
+def get_first5_ptags(article):
+    res = requests.get('https://en.wikipedia.org/wiki/' + article)
+    webpage_html = BeautifulSoup(res.content, 'html.parser')
+    first5para_ptags = webpage_html.select('div#mw-content-text div.mw-parser-output > p')[:5]
+    
+    return first5para_ptags
 
-    # Making the BeautifulSoup object
-    webpage = BeautifulSoup(article_html,'html.parser')
-    return webpage
-
-#webpage = get_html(article)
-
-def get_link(webpage_html):
-    # Finding the first paragraph using the logic defined in readme.md
-    article_bold_tag = webpage_html.select("p > b")[0]
-    first_para_tag = article_bold_tag.parent
-
-    # All the references inside the first paragraph
-    all_links = first_para_tag.find_all('a')
-
-    # The text in the first paragraph
-    para_text = first_para_tag.text
-
-    # Regex used to remove any links that are inside () or []
-    forbidden_text_regex = re.compile('\(.*?\)|\[.*?\]')
-    forbidden_text_list = forbidden_text_regex.findall(para_text)
-
-    # Replacing the text inside brackets from the paragraph text
-    for forbidden_text in forbidden_text_list:
-        para_text = para_text.replace(forbidden_text,'')
-
-    # The text present inside the links
-    link_texts = [link.text for link in all_links]
-
-    word_found = False
-    word = None
-    # Printing the text for the link
-    for link_text in link_texts:
-        if link_text in para_text:
-            word_found = True
-            word = link_text
-            #word = link_text
-            break
-
-    # Printing the url for the link for further use
+def get_all_links(ptags):
+    links_dict={}
+    all_links = []
+    for para in ptags:
+        all_links_in_para = para.find_all('a')
+        all_links.extend(all_links_in_para)
+    
     for link in all_links:
-        if link.text == word:
-            word = link
-            #print(link['href'])
+        try:
+            link_key = link.get_text(" ", strip=True)
+            link_value = link['href']
+            links_dict[link_key] = link_value
+        except KeyError:
+            print(link)
+            
+    return links_dict,all_links
 
-    if word_found:
-        return (word['href'],word_found)
-    else:
-        print('Word not found')
-        return None
+def get_para_text(ptags):
+    first5para_text = ''
+    for para in ptags:
+        first5para_text = first5para_text + para.get_text(" ", strip=True)
+        
+    return first5para_text
+
+def get_clean_text(first5para_text):
+    bracket_count = 0
+    clean_text = ''
+    for i in first5para_text:
+        if i == '(' or i == '[':
+            bracket_count += 1
+        if i == ')' or i == ']':
+            bracket_count -= 1
+        if bracket_count == 0:
+            clean_text += i
+            
+    return clean_text
+
+def get_next_link(links, clean_text):
+    clean_links = {}
+    for index, key in enumerate(links):
+        if key in clean_text and key != '':
+            try:
+                pos = clean_text.index(' '+key+' ')
+                clean_links[pos] = key
+            except ValueError:
+                pass
+            
+    ans_key = clean_links[min(clean_links.keys())]
+    ans = links[ans_key]
+    return ans
+
+
 
 if __name__ == '__main__':
-    # The article that we are going to search for.
-    article = 'Creativity'
-    webpage_html = get_html(article)
-    link = get_link(webpage_html)
-    print('https://en.wikipedia.org'+link[0])
+    article = 'Anime'
+    print('/wiki/'+article)
+    first5_ptags = get_first5_ptags(article)
+    links_dict,dirty_links = get_all_links(first5_ptags)
+    dirty_text = get_para_text(first5_ptags)
+    clean_text = get_clean_text(dirty_text)
+    next_link = get_next_link(links_dict, clean_text)
+    print(next_link)
+    while next_link != '/wiki/Outline_of_philosophy':
+        next_article = next_link[6:]
+        first5_ptags = get_first5_ptags(next_article)
+        links_dict,dirty_links = get_all_links(first5_ptags)
+        dirty_text = get_para_text(first5_ptags)
+        clean_text = get_clean_text(dirty_text)
+        next_link = get_next_link(links_dict, clean_text)
+        print(next_link)
+
+    print('/wiki/Philosophy')
+
+    
+    
